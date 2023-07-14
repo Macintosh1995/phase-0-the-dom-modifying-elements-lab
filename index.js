b2631afc1de1f0858939a75fc4f531c2dc6a6159
Daniel <danmcdonald95@outlook.com>
2023-07-13 23:48:35 -0700

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const remove =document.getElementById("main");
+const toa=remove.remove()
+
+const newHeader =document.createElement("h1")
+
+newHeader.setAttribute('id','victory')
+newHeader.textContent="Kimemia is the champion";
+
+document.body.append(newHeader)
